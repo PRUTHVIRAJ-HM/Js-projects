02eaefcce8f77bc42168184dc50e2258476360e9
Pruthviraj HM <120030137+PRUTHVIRAJ-HM@users.noreply.github.com>
2024-09-07 10:36:20 +0530

    a new joke generator first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Java_Script
+## Joke-Generator
+
---
+++ script.js
@@ -0,0 +1,182 @@
+function runcode(){
+    var sol = Math.floor(Math.random()*56)+1;
+    console.log(sol);
+    document.getElementById("content-intro").innerHTML="";
+    document.getElementById("content-image").src="./images/joking.png";
+
+    switch (sol) {
+        case 1:
+            document.getElementById("answer").innerHTML = "Why don't skeletons fight each other? They don't have the guts!";
+            break;
+        case 2:
+            document.getElementById("answer").innerHTML = "What do you call fake spaghetti? An impasta!";
+            break;
+        case 3:
+            document.getElementById("answer").innerHTML = "Why did the bicycle fall over? It was two-tired!";
+            break;
+        case 4:
+            document.getElementById("answer").innerHTML = "Why don’t scientists trust atoms? Because they make up everything!";
+            break;
+        case 5:
+            document.getElementById("answer").innerHTML = "I would tell you a joke about an elevator, but it’s an uplifting experience!";
+            break;
+        case 6:
+            document.getElementById("answer").innerHTML = "How does the ocean say hi? It waves!";
+            break;
+        case 7:
+            document.getElementById("answer").innerHTML = "What do you call cheese that isn't yours? Nacho cheese!";
+            break;
+        case 8:
+            document.getElementById("answer").innerHTML = "Why don’t eggs tell jokes? They’d crack each other up!";
+            break;
+        case 9:
+            document.getElementById("answer").innerHTML = "Why did the golfer bring two pairs of pants? In case he got a hole in one!";
+            break;
+        case 10:
+            document.getElementById("answer").innerHTML = "What do you call a belt made of watches? A waist of time!";
+            break;
+        case 11:
+            document.getElementById("answer").innerHTML = "Why do cows wear bells? Because their horns don't work!";
+            break;
+        case 12:
+            document.getElementById("answer").innerHTML = "What do you call a snowman with a six-pack? An abdominal snowman!";
+            break;
+        case 13:
+            document.getElementById("answer").innerHTML = "Why did the scarecrow win an award? Because he was outstanding in his field!";
+            break;
+        case 14:
+            document.getElementById("answer").innerHTML = "What do you get when you cross a snowman and a vampire? Frostbite!";
+            break;
+        case 15:
+            document.getElementById("answer").innerHTML = "How do you make a tissue dance? You put a little boogie in it!";
+            break;
+        case 16:
+            document.getElementById("answer").innerHTML = "Why don’t skeletons fight each other? They don’t have the guts!";
+            break;
+        case 17:
+            document.getElementById("answer").innerHTML = "Why don’t some couples go to the gym? Because some relationships don’t work out!";
+            break;
+        case 18:
+            document.getElementById("answer").innerHTML = "Why did the computer go to the doctor? It had a virus!";
+            break;
+        case 19:
+            document.getElementById("answer").innerHTML = "Why do ducks make great detectives? They always quack the case!";
+            break;
+        case 20:
+            document.getElementById("answer").innerHTML = "What do you call a dog magician? A labracadabrador!";
+            break;
+        case 21:
+            document.getElementById("answer").innerHTML = "Why was the math book sad? It had too many problems!";
+            break;
+        case 22:
+            document.getElementById("answer").innerHTML = "Why did the tomato turn red? Because it saw the salad dressing!";
+            break;
+        case 23:
+            document.getElementById("answer").innerHTML = "What did the janitor say when he jumped out of the closet? Supplies!";
+            break;
+        case 24:
+            document.getElementById("answer").innerHTML = "Why did the bicycle fall over? It was two-tired!";
+            break;
+        case 25:
+            document.getElementById("answer").innerHTML = "Why did the chicken join a band? Because it had the drumsticks!";
+            break;
+        case 26:
+            document.getElementById("answer").innerHTML = "How do you organize a space party? You planet!";
+            break;
+        case 27:
+            document.getElementById("answer").innerHTML = "What do you call a factory that makes good products? A satisfactory!";
+            break;
+        case 28:
+            document.getElementById("answer").innerHTML = "Why was the big cat disqualified from the race? Because it was a cheetah!";
+            break;
+        case 29:
+            document.getElementById("answer").innerHTML = "What’s orange and sounds like a parrot? A carrot!";
+            break;
+        case 30:
+            document.getElementById("answer").innerHTML = "Why don't you ever see elephants hiding in trees? Because they're so good at it!";
+            break;
+        case 31:
+            document.getElementById("answer").innerHTML = "What did one hat say to the other? Stay here, I’m going on ahead!";
+            break;
+        case 32:
+            document.getElementById("answer").innerHTML = "Why did the picture go to jail? It was framed!";
+            break;
+        case 33:
+            document.getElementById("answer").innerHTML = "What do you get when you cross a sheep and a kangaroo? A woolly jumper!";
+            break;
+        case 34:
+            document.getElementById("answer").innerHTML = "Why can’t your nose be 12 inches long? Because then it would be a foot!";
+            break;
+        case 35:
+            document.getElementById("answer").innerHTML = "Why don’t you ever see pigs hiding in trees? Because they aren’t very good at it!";
+            break;
+        case 36:
+            document.getElementById("answer").innerHTML = "What time did the man go to the dentist? Tooth-hurty!";
+            break;
+        case 37:
+            document.getElementById("answer").innerHTML = "Why don't some fish play piano? Because you can’t tuna fish!";
+            break;
+        case 38:
+            document.getElementById("answer").innerHTML = "What’s brown and sticky? A stick!";
+            break;
+        case 39:
+            document.getElementById("answer").innerHTML = "Why was the stadium so cool? It was filled with fans!";
+            break;
+        case 40:
+            document.getElementById("answer").innerHTML = "What do you call a fish wearing a crown? A king fish!";
+            break;
+        case 41:
+            document.getElementById("answer").innerHTML = "Why did the orange stop? It ran out of juice!";
+            break;
+        case 42:
+            document.getElementById("answer").innerHTML = "Why did the banana go to the doctor? It wasn’t peeling well!";
+            break;
+        case 43:
+            document.getElementById("answer").innerHTML = "What did one wall say to the other wall? I'll meet you at the corner!";
+            break;
+        case 44:
+            document.getElementById("answer").innerHTML = "How do you catch a squirrel? Climb a tree and act like a nut!";
+            break;
+        case 45:
+            document.getElementById("answer").innerHTML = "Why don’t you ever see hippos hiding in trees? Because they’re really good at it!";
+            break;
+        case 46:
+            document.getElementById("answer").innerHTML = "Why did the cookie go to the hospital? Because it felt crummy!";
+            break;
+        case 47:
+            document.getElementById("answer").innerHTML = "Why don't oysters share their pearls? Because they’re shellfish!";
+            break;
+        case 48:
+            document.getElementById("answer").innerHTML = "Why can’t you give Elsa a balloon? Because she’ll let it go!";
+            break;
+        case 49:
+            document.getElementById("answer").innerHTML = "Why don’t cows have any money? Because the farmers milk them dry!";
+            break;
+        case 50:
+            document.getElementById("answer").innerHTML = "Why did the man put his money in the blender? Because he wanted to make liquid assets!";
+            break;
+        case 51:
+            document.getElementById("answer").innerHTML = "What kind of shoes do ninjas wear? Sneakers!";
+            break;
+        case 52:
+            document.getElementById("answer").innerHTML = "Why did the girl bring a ladder to school? Because she wanted to go to high school!";
+            break;
+        case 53:
+            document.getElementById("answer").innerHTML = "How do cows stay up to date? They read the moos-paper!";
+            break;
+        case 54:
+            document.getElementById("answer").innerHTML = "Why don’t koalas count as bears? They don’t have the right koalafications!";
+            break;
+        case 55:
+            document.getElementById("answer").innerHTML = "Why did the golfer bring extra socks? In case he gets a hole in one!";
+            break;
+        case 56:
+            document.getElementById("answer").innerHTML = "What do you call a sleeping bull? A bulldozer!";
+            break;
+    }
+    
+}
+
+function refreshing(){
+    location.reload();
+}
